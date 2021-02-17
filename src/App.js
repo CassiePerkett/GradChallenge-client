710d3918312fc20d762bf68541dc57171026fc73
CassiePerkett <cassperkett@gmail.com>
2021-02-17 08:11:54 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: palevioletred;
+  background-color: white;
+  font-size: 1em;
+  margin: 1em;
+  border: 2px solid;
+  border-color: palevioletred;
+  padding: 0.25em 1em;
+  cursor: pointer;
+  border-radius: 3px;
+  
+  &:hover {
+    background-color: salmon;
+    color: black;
+  }
+`;
+
+const Section = styled.div`
+  color: white;
+  background: #7F00FF;
+  background: linear-gradient(to right, #E100FF, #7F00FF);
+  padding: 2em 1em;
+  font-size: 1.5em;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Section>
+      <h1>Cassie's Crypto Counter</h1>
+      </Section>
+      <br></br>
+      <p>Crytocurrency coin information will be listed below.</p>
+      <br></br>
+      <Button>I am a button</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,80 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+//import ApolloBoost, { gql } from "apollo-boost";
+//import { ApolloProvider } from '@apollo/client';
+//import { InMemoryCache } from 'apollo-cache-inmemory';
+//import { RestLink } from 'apollo-link-rest';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// setup your `RestLink` with your endpoint
+/*const restLink = new RestLink({ 
+  uri: "http://localhost:4000/" 
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//setup new client
+const client = new ApolloClient({
+  link: restLink,
+  cache: new InMemoryCache(),
+});
+
+const getCoins = gql`
+query{ 
+  coins{
+    id
+    symbol
+    name
+  }
+}
+
+`
+client.query({
+  query: getCoins
+}).then ((response) => {
+  console.log(response.data)
+})
+*/
+
+/*
+client
+  .query({
+    query: gql`
+      query GetAllCoin {
+        GetCoin(id: String) {
+          id
+          symbol
+          name
+          nameid
+          rank
+          price_usd
+          percent_change_24h
+          percent_change_1h
+          percent_change_7d
+          price_btc
+          market_cap_usd
+          volume24
+          volume24a
+          csupply
+          tsupply
+          msupply
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));*/
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+
+//Questionswhen setting up client
+//1. How do we define an operation in js
+//2. How do we send off to the server to fetch a response
+//3. How do we access the response
